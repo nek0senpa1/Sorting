337a301207b45be0319f78cc6d2706d01fdc7ec6
amberm <nekosenpai@gmail.com>
2019-05-08 10:56:42 -0600

    was missing a ='s ...

--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -1,6 +1,6 @@
 # TO-DO: complete the helpe function below to merge 2 sorted arrays
-stuff = [1,2,2,3,46,74968,4,984,64,3,63510351,85,435,4,8543,8543,613,2,1]
-stuff2 = [11,12,2,13,146,74968,14,984,164,13,85,1435,114,8543,613,21,11]
+stuff = [1,2,3,4,5,6,7,8,9,10]
+stuff2 = [1,2,3,4,5,6,7,8,9,10]
 
 
 def merge( arrA, arrB ):
@@ -15,16 +15,23 @@ def merge( arrA, arrB ):
         if arrA[thing1] < arrB[thing2]:
             merged_arr[i] = arrA[thing1]
 
-            if thing1 <= len(arrA)-1:
+            if thing1 < len(arrA)-1:
                 thing1 = thing1 +1
-                print('got to thing1a', thing1)
+                print('got to thing1a', merged_arr)
 
         elif arrB[thing2] <= arrA[thing1]:
             merged_arr[i] = arrB[thing2]
 
-            if thing2 <= len(arrB)-1:
+            if thing2 < len(arrB)-1:
                 thing2 = thing2 +1
-                print('got to thing2a', thing2)
+                print('got to thing2a', merged_arr)
+
+        elif arrA[thing1] == arrB[thing2]:
+            merged_arr[i] = arrB[thing2]
+
+            if thing2 < len(arrB):
+                thing2 = thing2 +1
+                print('got to thing2a equaled!!', merged_arr)
         
         elif thing1 >= len(arrA):
             merged_arr[i] = arrB[thing2]
@@ -46,6 +53,12 @@ print(merge(stuff,stuff2))
 # TO-DO: implement the Merge Sort function below USING RECURSION
 def merge_sort( arr ):
     # TO-DO
+    mid = len(arr)//2
+
+    if len(arr):
+        right = merge_sort(arr[mid:])
+        left = merge_sort(arr[:mid])
+        arr = merge(left,right)
 
     return arr
 
