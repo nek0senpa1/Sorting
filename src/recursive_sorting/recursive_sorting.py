a1b0684110bf6e19523b57843e603d2297308e3b
amberm <nekosenpai@gmail.com>
2019-05-08 09:50:41 -0600

    index out of range issue

--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -1,6 +1,6 @@
 # TO-DO: complete the helpe function below to merge 2 sorted arrays
 stuff = [1,2,2,3,46,74968,4,984,64,3,63510351,85,435,4,8543,8543,613,2,1]
-stuff2 = [11,12,2,13,146,74968,14,984,164,13,63510351,85,1435,114,8543,8543,613,21,11]
+stuff2 = [11,12,2,13,146,74968,14,984,164,13,85,1435,114,8543,613,21,11]
 
 
 def merge( arrA, arrB ):
@@ -11,15 +11,32 @@ def merge( arrA, arrB ):
     thing2 = 0
     # print(merged_arr)
     for i in range(0, elements):
-        if thing1 >= arrA[thing2]:
+        
+        if arrA[thing1] < arrB[thing2]:
+            merged_arr[i] = arrA[thing1]
+
+            if thing1 <= len(arrA)-1:
+                thing1 = thing1 +1
+                print('got to thing1a', thing1)
+
+        elif arrB[thing2] <= arrA[thing1]:
+            merged_arr[i] = arrB[thing2]
+
+            if thing2 <= len(arrB)-1:
+                thing2 = thing2 +1
+                print('got to thing2a', thing2)
+        
+        elif thing1 >= len(arrA):
             merged_arr[i] = arrB[thing2]
             thing2 = thing2+1
             print('got to thing2', thing2)
         
-        elif thing2 >= arrB[thing1]:
+        elif thing2 >= len(arrB):
             merged_arr[i] = arrA[thing1]
             thing1 = thing1 +1
             print('got to thing1', thing1)
+
+        
 
 
     return merged_arr
